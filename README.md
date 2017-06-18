78396942e395ecff1caf2a1112e8e3f24a96959c
Michael Conlon <mconlon@ufl.edu>
2017-06-17 22:29:18 -0400

    Basic functionality in place for Vitro

--- README.md
+++ README.md
@@ -9,4 +9,13 @@ Getting started to produce a catalog of templates for Vitro and VIVO.
     1. Path/name
     1. "tree" -- a depiction of what the template calls and what is called by those things on down to the leafs
     1. language --- an attempt to discern if the template contains hard coded language, or appears to be i18n clean
-    1. calls -- how many times is the template called.  Helps us find important templates, and templates we might be able to remove
+    1. i18n count -- how many times does the template refer to i18n.  Helps with translation
+    1. called -- how many times is the template called.  Helps us find important templates, and templates we might be able to remove
+    1. calls -- how many calls does the template make to other templates
+    
+## To do
+
+Process the templates as in the running system:
+
+    * if vitro theme, process the vito templates, then process the vitro theme templates
+    * if vivo theme, process vitro, then vivo, then vivo theme
--- template-catalog.py
+++ template-catalog.py
@@ -9,14 +9,12 @@
 
 """
 
-# TODO: improve handling of comments in the templates
-# TODO: Add code for recursive tree of calls for each template
-# TODO: Add counts for calls for each template
+# TODO: To count VIVO, process Vitro and then VIVO.  Add then templates
 
 __author__ = "Michael Conlon"
 __copyright__ = "Copyright (c) 2017 Michael Conlon"
 __license__ = "Apache-2 license"
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 
 
 def file_len(fname):
@@ -31,6 +29,33 @@ def file_len(fname):
     return i + 1
 
 
+def file_count_i18n(fname):
+    """
+    Determine the number of lines in a file
+    :param fname:
+    :return:
+    """
+    with open(fname) as f:
+        data = f.read()
+        return data.count('${i18n().')
+
+
+def file_tree(ftl, name, indent=0):
+    """
+    given the ftl structure and a name, print the file tree for the name.  For each name called,
+    print the file tree (recursive)
+    :param ftl:
+    :param name:
+    :return:
+    """
+    if ftl[name]['ncalls'] == 0:
+        return
+    indent += 4
+    for nm in ftl[name]['calls']:
+        print ' ' * indent, nm
+        file_tree(ftl, nm, indent)
+
+
 def file_calls(fname):
     """
     Determine the templates called by this template
@@ -39,8 +64,14 @@ def file_calls(fname):
     """
     calls = set()
     with open(fname) as f:
+        skip = False
         for i, l in enumerate(f):
             if '<#--' in l:
+                skip = True
+            if '-->' in l:
+                skip = False
+                continue
+            if skip:
                 continue
             if '.ftl' in l:
                 end = l.find('.ftl')
@@ -52,6 +83,38 @@ def file_calls(fname):
     return calls
 
 
+def file_has_text(fname):
+    """
+    Determine the templates called by this template
+    :param fname:
+    :return:
+    """
+    import re
+    has_text = False
+    with open(fname) as f:
+        skip = False
+        for i, l in enumerate(f):
+            if '<#--' in l or '<!--' in l or '<script' in l:
+                skip = True
+            match = re.search(r'<#[a-z]', l)
+            if match:
+                continue
+            if '<@' in l:
+                continue
+            if '-->' in l or '</script>' in l:
+                skip = False
+                continue
+            if skip:
+                continue
+
+            match = re.search(r' [a-z]+ ', l)
+            if match:
+                has_text = True
+                print fname, "text = ", l
+                break
+    return has_text
+
+
 def main():
     """
     The main function.  Does the work
@@ -59,18 +122,34 @@ def main():
     """
     import os
 
-    root_dir = '/Users/mikeconlon/PycharmProjects/VIVO/webapp/src'
+    root_dir = '/Users/mikeconlon/PycharmProjects/Vitro/webapp/src'
     count = 0
+    ftl = {}
 
     for directory, subdirectories, files in os.walk(root_dir):
         for file_name in files:
             if file_name.endswith('.ftl'):
                 path_name = os.path.join(directory, file_name)
-                print file_len(path_name), file_name, file_calls(path_name)
+                calls = file_calls(path_name)
                 count += 1
+                ftl[file_name] = {}
+                ftl[file_name]['lines'] = file_len(path_name)
+                ftl[file_name]['path'] = path_name
+                ftl[file_name]['ncalls'] = len(calls)
+                ftl[file_name]['calls'] = calls
+                ftl[file_name]['called'] = 0
+                ftl[file_name]['has_text'] = file_has_text(path_name)
+                ftl[file_name]['i18n'] = file_count_i18n(path_name)
+
+    for file_name in ftl:
+        for name in ftl[file_name]['calls']:
+            ftl[name]['called'] += 1
 
     print "\n", count, "templates"
-
+    for name in ftl:
+        print name, 'has text = ', ftl[name]['has_text'], 'i18n count =', ftl[name]['i18n'], 'lines =', \
+            ftl[name]['lines']
+        file_tree(ftl, name)
 
 if __name__ == "__main__":
     main()
